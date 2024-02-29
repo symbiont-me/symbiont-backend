adf9ccb560e858b2037b4ac3c8d544d8cfd87903
lonehegelian <thelonehegelian@protonmail.com>
2024-02-29 21:14:12 +0000

    Separate download_from_firebase_storage and delete_local_file functions

--- symbiont/src/fb/storage.py
+++ symbiont/src/fb/storage.py
@@ -1,6 +1,9 @@
 # from fastapi import FastAPI, File, UploadFile
 # from firebase_admin import initialize_app, storage, credentials
 
+import requests
+import os
+
 # cred = credentials.Certificate("src/serviceAccountKey.json")
 # initialize_app(cred, {"storageBucket": "your_bucket_link_without_gs://"})
 
@@ -13,3 +16,28 @@
 
 #     public_url = f"https://storage.googleapis.com/{bucket.name}/{blob.name}"
 #     return public_url
+
+
+async def download_from_firebase_storage(
+    file_key: str, download_url: str
+) -> str | None:
+    try:
+        response = requests.get(download_url)
+        response.raise_for_status()
+        save_path = f"temp/{file_key}"
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        with open(save_path, "wb") as f:
+            f.write(response.content)
+        print(f"File downloaded successfully and saved to {save_path}")
+        return save_path
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP Error occurred: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return None
+
+
+# TODO maybe delete the entire directory because we do not want to keep the user id in the file path
+async def delete_local_file(file_path: str):
+    os.remove(file_path)
+    print(f"File deleted successfully from {file_path}")
