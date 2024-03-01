436ab8b8ea41b9e248dbf8b4b8f4825b4195386d
lonehegelian <thelonehegelian@protonmail.com>
2024-03-01 23:21:55 +0000

    Add study_test.py with TODOs for combining resources and creating a test paper

---
+++ symbiont/src/routers/study_test.py
@@ -0,0 +1,3 @@
+# TODO combine the contents of all the resources
+# TODO make an MCQ style test paper with 20-25 questions
+# TODO make a list of 10 questions the student should be able to answer after thoroughly researching a topic
