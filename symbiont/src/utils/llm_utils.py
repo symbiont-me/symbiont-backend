a0727de9cb0e73e0224a59a809f80bcf340468c1
lonehegelian <thelonehegelian@protonmail.com>
2024-03-04 20:16:54 +0000

    refactor get summary to update db in case of error

--- symbiont/src/utils/llm_utils.py
+++ symbiont/src/utils/llm_utils.py
@@ -10,15 +10,21 @@ llm = OpenAI(temperature=0, name=LLMModel.GPT_3_5_TURBO_16K)
 
 # NOTE used for webpages and plain text
 def summarise_plain_text_resource(document_text):
+    summary = ""
+    try:
+        nltk_text_splitter = NLTKTextSplitter()
 
-    nltk_text_splitter = NLTKTextSplitter()
+        docs = nltk_text_splitter.create_documents([document_text])
 
-    docs = nltk_text_splitter.create_documents([document_text])
-
-    summary_chain = load_summarize_chain(
-        llm=llm,
-        chain_type="map_reduce",
-        verbose=True,  # Set verbose=True if you want to see the prompts being used
-    )
-    summaries = summary_chain.run(docs)
-    return summaries
+        summary_chain = load_summarize_chain(
+            llm=llm,
+            chain_type="map_reduce",
+            verbose=False,  # Set verbose=True if you want to see the prompts being used
+        )
+        summary = summary_chain.run(docs)
+        return summary
+    except Exception as e:
+        # Handle the exception here
+        print(f"An error occurred: {e}")
+        summary = f"An error occurred: Summary could not be generated."
+    return summary
