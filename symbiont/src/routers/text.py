8e14fb35a06539ad41a6b1ac34a2e455c1fd84ce
lonehegelian <thelonehegelian@protonmail.com>
2024-03-16 13:46:05 +0000

    Refactor code to use StudyService in text router

--- symbiont/src/routers/text.py
+++ symbiont/src/routers/text.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, HTTPException
 from ..models import TextUpdateRequest
 from firebase_admin import firestore
-from ..utils.db_utils import get_document_dict, get_document_ref
+from ..utils.db_utils import StudyService
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       STUDY TEXT
@@ -13,7 +13,8 @@ router = APIRouter()
 @router.get("/get-text")
 async def get_text(study_id: str, request: Request):
     user_uid = request.state.verified_user["user_id"]
-    study_dict = get_document_dict("studies_", "userId", user_uid, study_id)
+    study_service = StudyService(user_uid, study_id)
+    study_dict = study_service.get_document_dict()
     if study_dict is None:
         raise HTTPException(status_code=404, detail="Study not found")
     return {"text": study_dict.get("text", "")}
@@ -22,7 +23,8 @@ async def get_text(study_id: str, request: Request):
 @router.post("/update-text")
 async def update_text(text: TextUpdateRequest, request: Request):
     user_uid = request.state.verified_user["user_id"]
-    study_ref = get_document_ref("studies_", "userId", user_uid, text.studyId)
+    study_service = StudyService(user_uid, text.studyId)
+    study_ref = study_service.get_document_ref()
     if study_ref is None:
         raise HTTPException(status_code=404, detail="No such document!")
     study = study_ref.get()
