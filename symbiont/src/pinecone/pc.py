e38f2f13e8c01651ec6ff3006d102537ac9b91e3
lonehegelian <thelonehegelian@protonmail.com>
2024-03-16 13:45:31 +0000

    refactor to use a Service class for handling user context

--- symbiont/src/pinecone/pc.py
+++ symbiont/src/pinecone/pc.py
@@ -19,9 +19,6 @@ from . import pc_index
 from ..models import EmbeddingModels
 from firebase_admin import firestore
 
-# ~~~~~~~~~~~~~~~~~~~~
-#       PINECONE
-# ~~~~~~~~~~~~~~~~~~~~
 
 load_dotenv()
 
@@ -32,219 +29,194 @@ pinecone_api_key = os.getenv("PINECONE_API_KEY")
 pinecone_index = os.getenv("PINECONE_INDEX")
 pinecone_endpoint = os.getenv("PINECONE_API_ENDPOINT")
 
-# Initialize the OpenAIEmbeddings object, will use the same object for embedding tasks
-embed = OpenAIEmbeddings(model=EmbeddingModels.TEXT_EMBEDDING_3_LARGE, dimensions=1536)
 
-# embed = CohereEmbeddings(
-#     model="embed-english-light-v3.0",
-# )
+class PineconeService:
+    def __init__(
+        self, user_uid=None, user_query=None, resource_identifier=None, study_id=None
+    ):
 
-
-def delete_vectors_from_pinecone(namespace):
-    try:
-        if pc_index:
-            pc_index.delete(delete_all=True, namespace=namespace)
-            print(f"Deleted namespace {namespace}")
-    except Exception as e:
-        print(f"Error deleting namespace {namespace}: {str(e)}")
-
-
-# make this generic it should take various types of resources
-async def embed_document(doc: Union[DocumentPage, Document]) -> PineconeRecord:
-
-    vec = await embed.aembed_query(doc.page_content)
-    hash = md5(doc.page_content.encode("utf-8")).hexdigest()
-    create_vec_ref_in_db("user_uid", hash, doc.metadata)
-    return PineconeRecord(id=hash, values=vec, metadata=doc.metadata)
-
-
-async def handle_pdf_resource(file_path: str):
-    if file_path is not None and file_path.endswith(".pdf"):
-        loader = PyPDFLoader(file_path)
-        pages = [DocumentPage(**page.dict()) for page in loader.load_and_split()]
-
-        docs = []
-        for page in pages:
-            prepared_pages = await prepare_pdf_for_pinecone(page)
-            docs.extend(prepared_pages)
-        vecs = [await embed_document(doc) for doc in docs]
-        return vecs
-    return []
-
-
-async def prepare_resource_for_pinecone(file_identifier: str, download_url: str):
-    file_path = await download_from_firebase_storage(file_identifier, download_url)
-    # handle pdf only for now
-    if file_path is not None and file_path.endswith(".pdf"):
-        vecs = await handle_pdf_resource(file_path)
-
-        await upload_vecs_to_pinecone(vecs, file_identifier)
-        await delete_local_file(file_path)
-
-
-# TODO rename this function as it is used for more than just webpages
-async def upload_webpage_to_pinecone(resource, content):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=20,
-        length_function=len,
-        is_separator_regex=False,
-    )
-    split_texts = text_splitter.create_documents([content])
-    docs = [
-        DocumentPage(
-            page_content=split_text.page_content,
-            metadata={
-                "text": split_text.page_content,
-                "source": resource.identifier,
-                "page": 0,  # there are no pages in a webpage or plain text
-            },
-            type=resource.category,
+        self.user_uid = user_uid
+        self.user_query = user_query
+        self.resource = resource_identifier
+        self.study_id = study_id
+        self.db = firestore.client()
+        self.embed = OpenAIEmbeddings(
+            model=EmbeddingModels.TEXT_EMBEDDING_3_LARGE, dimensions=1536
         )
-        for split_text in split_texts
-    ]
+        # Other initializations...
 
-    vecs = [await embed_document(doc) for doc in docs]
-    await upload_vecs_to_pinecone(vecs, resource.identifier)
+    def get_vectors_from_db(self):
+        vec_ref = self.db.collection("users").document(self.user_uid)
+        vec_data = vec_ref.get().to_dict()
+        if "vectors" not in vec_data:
+            return None
+        return vec_data["vectors"]
 
+    def create_vec_ref_in_db(self, md5_hash, metadata):
+        db = firestore.client()
+        vec_ref = db.collection("users").document(self.user_uid)
+        # Retrieve the current data to avoid overwriting
+        current_data = vec_ref.get().to_dict()
+        # Initialize 'vectors' as a mapping if it doesn't exist
+        if "vectors" not in current_data:
+            current_data["vectors"] = {}
+        # Update the mapping with the new vector data using md5 as the key
+        current_data["vectors"][md5_hash] = metadata
+        # Update the document with the new mapping of vectors
+        vec_ref.set(current_data)
+        return vec_ref
 
-def create_vec_ref_in_db(user_uid, md5_hash, metadata):
-    db = firestore.client()
-    vec_ref = db.collection("users").document(user_uid)
-    # Retrieve the current data to avoid overwriting
-    current_data = vec_ref.get().to_dict()
-    # Initialize 'vectors' as a mapping if it doesn't exist
-    if "vectors" not in current_data:
-        current_data["vectors"] = {}
-    # Update the mapping with the new vector data using md5 as the key
-    current_data["vectors"][md5_hash] = metadata
-    # Update the document with the new mapping of vectors
-    vec_ref.set(current_data)
-    return vec_ref
+    # make this generic it should take various types of resources
+    async def embed_document(
+        self, doc: Union[DocumentPage, Document]
+    ) -> PineconeRecord:
+        vec = await self.embed.aembed_query(doc.page_content)
+        hash = md5(doc.page_content.encode("utf-8")).hexdigest()
+        self.create_vec_ref_in_db(hash, doc.metadata)
+        return PineconeRecord(id=hash, values=vec, metadata=doc.metadata)
 
+    def delete_vectors_from_pinecone(self, namespace):
+        try:
+            if pc_index:
+                pc_index.delete(delete_all=True, namespace=namespace)
+                print(f"Deleted namespace {namespace}")
+        except Exception as e:
+            print(f"Error deleting namespace {namespace}: {str(e)}")
 
-async def upload_yt_resource_to_pinecone(resource, content):
-    # NOTE this is causing problems, it seems to be cutting off the text
-    # content = truncate_string_by_bytes(content, 10000)
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=20,
-        length_function=len,
-        is_separator_regex=False,
-    )
-    split_texts = text_splitter.create_documents([content])
-    # NOTE I should be able to use the Document from langchain_core.documents everywhere
-    docs = [
-        DocumentPage(
-            page_content=split_text.page_content,
-            metadata={
-                "text": split_text.page_content,
-                "source": resource.identifier,
-                "page": 0,  # there are no pages in a youtube video
-            },
-            type=resource.category,
+    async def handle_pdf_resource(self, file_path: str):
+        if file_path is not None and file_path.endswith(".pdf"):
+            loader = PyPDFLoader(file_path)
+            pages = [DocumentPage(**page.dict()) for page in loader.load_and_split()]
+
+            docs = []
+            for page in pages:
+                prepared_pages = await self.prepare_pdf_for_pinecone(page)
+                docs.extend(prepared_pages)
+            vecs = [await self.embed_document(doc) for doc in docs]
+            return vecs
+        return []
+
+    async def prepare_resource_for_pinecone(
+        self, file_identifier: str, download_url: str
+    ):
+        file_path = await download_from_firebase_storage(file_identifier, download_url)
+        # handle pdf only for now
+        if file_path is not None and file_path.endswith(".pdf"):
+            vecs = await self.handle_pdf_resource(file_path)
+
+            await self.upload_vecs_to_pinecone(vecs, file_identifier)
+            await delete_local_file(file_path)
+
+    # TODO rename this function as it is used for more than just webpages
+    async def upload_webpage_to_pinecone(self, resource, content):
+        text_splitter = NLTKTextSplitter()
+        split_texts = text_splitter.create_documents([content])
+        docs = [
+            DocumentPage(
+                page_content=split_text.page_content,
+                metadata={
+                    "text": split_text.page_content,
+                    "source": resource.identifier,
+                    "page": 0,  # there are no pages in a webpage or plain text
+                },
+                type=resource.category,
+            )
+            for split_text in split_texts
+        ]
+
+        vecs = [await self.embed_document(doc) for doc in docs]
+        await self.upload_vecs_to_pinecone(vecs, resource.identifier)
+
+    def get_chat_context(self, top_k=2):
+        if self.resource is None or self.user_query is None:
+            raise ValueError(
+                "Resource and user query must be provided to get chat context"
+            )
+        context = ""
+        pc_results = self.search_pinecone_index(self.user_query, self.resource, top_k)
+
+        vec_metadata = []
+        for match in pc_results.matches:
+            vec_data = self.get_vectors_from_db()
+            if vec_data is None:
+                return ""
+            vec_metadata.append(vec_data[match.id])
+            context += vec_data[match.id]["text"]
+        print("CONTEXT", context)
+        return context
+
+    async def upload_yt_resource_to_pinecone(self, resource, content):
+        # NOTE this is causing problems, it seems to be cutting off the text
+        # content = truncate_string_by_bytes(content, 10000)
+        text_splitter = NLTKTextSplitter()
+        split_texts = text_splitter.create_documents([content])
+        # NOTE I should be able to use the Document from langchain_core.documents everywhere
+        docs = [
+            DocumentPage(
+                page_content=split_text.page_content,
+                metadata={
+                    "text": split_text.page_content,
+                    "source": resource.identifier,
+                    "page": 0,  # there are no pages in a youtube video
+                },
+                type=resource.category,
+            )
+            for split_text in split_texts
+        ]
+        vecs = [await self.embed_document(doc) for doc in docs]
+        await self.upload_vecs_to_pinecone(vecs, resource.identifier)
+
+    async def upload_vecs_to_pinecone(
+        self, vecs: List[PineconeRecord], file_identifier: str
+    ):
+        # TODO don't initialize Pinecone client here
+        metadata = (
+            {}
+        )  # TODO metadata is stored in the db. It should not be stored in Pinecone. Should have a better way to do this
+        formatted_vecs = [(vec.id, vec.values, metadata) for vec in vecs]
+        pc_index.upsert(vectors=formatted_vecs, namespace=file_identifier)
+        print("Uploaded to Pinecone")
+
+    def truncate_string_by_bytes(self, string, num_bytes):
+        encoded_string = string.encode("utf-8")
+        truncated_string = encoded_string[:num_bytes]
+        return truncated_string.decode("utf-8", "ignore")
+
+    async def prepare_pdf_for_pinecone(
+        self, pdf_page: DocumentPage
+    ) -> List[DocumentPage]:
+        page_content = pdf_page.page_content.replace("\n", "")
+        page_content = self.truncate_string_by_bytes(page_content, 10000)
+        text_splitter = NLTKTextSplitter()
+
+        split_texts = text_splitter.create_documents([page_content])
+        docs = [
+            DocumentPage(
+                page_content=split_text.page_content,
+                metadata={
+                    "text": split_text.page_content,
+                    "source": pdf_page.metadata["source"],
+                    "page": pdf_page.metadata["page"],
+                },
+                type=pdf_page.type,
+            )
+            for split_text in split_texts
+        ]
+
+        return docs
+
+    def get_query_embedding(self, query: str) -> List[float]:
+        vec = self.embed.embed_query(query)
+        return vec
+
+    def search_pinecone_index(self, query: str, file_identifier: str, top_k=2):
+
+        query_embedding = self.get_query_embedding(query)
+        query_matches = pc_index.query(
+            vector=query_embedding,
+            top_k=top_k,
+            namespace=file_identifier,
+            include_metadata=True,
         )
-        for split_text in split_texts
-    ]
-    vecs = [await embed_document(doc) for doc in docs]
-    await upload_vecs_to_pinecone(vecs, resource.identifier)
 
-
-async def upload_vecs_to_pinecone(vecs: List[PineconeRecord], file_identifier: str):
-    # TODO don't initialize Pinecone client here
-    client = Pinecone(api_key=pinecone_api_key, endpoint=pinecone_endpoint)
-    # TODO use this: pc_index.Index
-    index = client.Index("symbiont-me")
-    metadata = (
-        {}
-    )  # TODO metadata is stored in the db. It should not be stored in Pinecone. Should have a better way to do this
-    formatted_vecs = [(vec.id, vec.values, metadata) for vec in vecs]
-    if index is None:
-        raise Exception("Pinecone index not found")
-    index.upsert(vectors=formatted_vecs, namespace=file_identifier)
-    print("Uploaded to Pinecone")
-
-
-def truncate_string_by_bytes(string, num_bytes):
-    encoded_string = string.encode("utf-8")
-    truncated_string = encoded_string[:num_bytes]
-    return truncated_string.decode("utf-8", "ignore")
-
-
-async def prepare_pdf_for_pinecone(pdf_page: DocumentPage) -> List[DocumentPage]:
-    page_content = pdf_page.page_content.replace("\n", "")
-    page_content = truncate_string_by_bytes(page_content, 10000)
-    text_splitter = NLTKTextSplitter()
-    # text_splitter = RecursiveCharacterTextSplitter(
-    #     chunk_size=1000,
-    #     chunk_overlap=20,
-    #     length_function=len,
-    #     is_separator_regex=False,
-    # )
-
-    split_texts = text_splitter.create_documents([page_content])
-    docs = [
-        DocumentPage(
-            page_content=split_text.page_content,
-            metadata={
-                "text": split_text.page_content,
-                "source": pdf_page.metadata["source"],
-                "page": pdf_page.metadata["page"],
-            },
-            type=pdf_page.type,
-        )
-        for split_text in split_texts
-    ]
-
-    return docs
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#       Vector Search
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-def get_vector_data_from_db(user_uid, md5_hash):
-    db = firestore.client()
-    vec_ref = db.collection("users").document(user_uid)
-    vec_data = vec_ref.get().to_dict()
-    return vec_data["vectors"][md5_hash]
-
-
-def get_query_embedding(query: str) -> List[float]:
-    vec = embed.embed_query(query)
-    return vec
-
-
-def search_pinecone_index(query: str, file_identifier: str, top_k=2):
-    # TODO don't initialize Pinecone client here
-    # client = Pinecone(api_key=pinecone_api_key, endpoint=pinecone_endpoint)
-    # index = client.Index("symbiont-me")
-
-    # if index is None:
-    #     raise Exception("Pinecone index not found")
-
-    query_embedding = get_query_embedding(query)
-    query_matches = pc_index.query(
-        vector=query_embedding,
-        top_k=top_k,
-        namespace=file_identifier,
-        include_metadata=True,
-    )
-    search_results_from_db = []
-    for match in query_matches.matches:
-        vec_data = get_vector_data_from_db("user_uid", match.id)
-        search_results_from_db.append(vec_data)
-
-    return search_results_from_db
-
-
-def get_chat_context(query: str, file_identifier: str, top_k=25):
-    # TODO unpack the search_pinecone_index function into this function
-    results = search_pinecone_index(query, file_identifier)
-    context = ""
-    for vec_ref in results:
-        context += vec_ref["text"]
-    print(context)
-    # # TODO return an object with matches for detailed footnoting
-    return context
+        return query_matches
