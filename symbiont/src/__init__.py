768f0cec4b00e2d83bca42ae49567bb058541b52
lonehegelian <thelonehegelian@protonmail.com>
2024-03-20 21:23:28 +0000

    init logger at src

--- symbiont/src/__init__.py
+++ symbiont/src/__init__.py
@@ -1,7 +1,8 @@
-import firebase_admin
 import os
+from .utils.logger import Logger
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       OPENAI INIT
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 openai_api_key = os.getenv("OPENAI_API_KEY")
+logger_instance = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
