fdc87ee282d2843f43568c6d6b1079557ef38754
lonehegelian <thelonehegelian@protonmail.com>
2024-03-21 20:28:51 +0000

    initialise logger in main

--- symbiont/src/__init__.py
+++ symbiont/src/__init__.py
@@ -5,4 +5,4 @@ from .utils.logger import Logger
 #       OPENAI INIT
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 openai_api_key = os.getenv("OPENAI_API_KEY")
-logger_instance = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
+logger = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
