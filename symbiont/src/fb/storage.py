928296c7d1bb69f93f4c82bb022b0b83bf4143d2
lonehegelian <thelonehegelian@protonmail.com>
2024-03-21 22:34:48 +0000

    refactor to user resource_identifier only for downloading and deleting temp files

--- symbiont/src/fb/storage.py
+++ symbiont/src/fb/storage.py
@@ -1,25 +1,26 @@
 import requests
 import os
+from .. import logger
 
 
-async def download_from_firebase_storage(file_key: str, download_url: str):
+async def download_from_firebase_storage(resource_identifier: str, download_url: str):
     try:
         response = requests.get(download_url)
         response.raise_for_status()
-        save_path = f"temp/{file_key}"
+        save_path = f"temp/{resource_identifier}"
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, "wb") as f:
             f.write(response.content)
-        print(f"File downloaded successfully and saved to {save_path}")
+        logger.info(f"File downloaded successfully to {save_path}")
         return save_path
     except requests.exceptions.HTTPError as err:
-        print(f"HTTP Error occurred: {err}")
+        logger.error(f"HTTP error occurred: {err}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error(f"Error occurred: {e}")
     return None
 
 
 # TODO maybe delete the entire directory because we do not want to keep the user id in the file path
-async def delete_local_file(file_path: str):
-    os.remove(file_path)
-    print(f"File deleted successfully from {file_path}")
+async def delete_local_file(local_file_path: str):
+    os.remove(local_file_path)
+    logger.info(f"Deleted file {local_file_path}")
