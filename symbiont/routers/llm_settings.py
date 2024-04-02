0eedcb1b4c9a15bf9027e31c914cc19cefa0d76b
lonehegelian <thelonehegelian@protonmail.com>
2024-04-03 00:46:23 +0100

    update llm-settings route
    settings the api keys in the cookie and not saving them in db. Update settings save function

--- symbiont/routers/llm_settings.py
+++ symbiont/routers/llm_settings.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Cookie
 from pydantic import BaseModel
 from firebase_admin import firestore, auth, credentials, storage
-from fastapi import Request
+from fastapi import Request, Response
 from ..llms import get_user_llm_settings
 from ..utils.db_utils import StudyService
-
+from typing import Annotated
+from .. import logger
 
 router = APIRouter()
 
@@ -15,7 +16,31 @@ class LLMSettingsRequest(BaseModel):
 
 
 @router.post("/set-llm-settings")
-async def set_llm_settings(settings: LLMSettingsRequest, request: Request):
+async def set_llm_settings(
+    settings: LLMSettingsRequest,
+    request: Request,
+    response: Response,
+):
+    db = firestore.client()
     user_uid = request.state.verified_user["user_id"]
-    study_service = StudyService(user_uid)
-    return study_service.set_llm_settings(settings)
+    user_doc = db.collection("users").document(user_uid)
+    user_data = user_doc.get().to_dict()
+    if user_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    user_settings = user_data.get("settings")
+    if user_settings is None:
+        user_settings = {}
+
+    # @note don't save the api_key in the database
+    user_settings["llm_name"] = settings.llm_name
+    user_doc.update({"settings": user_settings})
+    response.set_cookie(
+        key="api_key", value=settings.api_key, samesite="None", secure=True
+    )
+    response.set_cookie(
+        key="llm_name", value=settings.llm_name, samesite="None", secure=True
+    )
+    return {"message": "LLM settings saved"}
