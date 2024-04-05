e938d2bb35c73b4d20165f01fe0418c55d5050e9
lonehegelian <thelonehegelian@protonmail.com>
2024-04-05 09:00:34 +0100

    Chores: raise error if environment is not set

--- symbiont/__init__.py
+++ symbiont/__init__.py
@@ -1,5 +1,8 @@
 import os
 from .utils.logger import Logger
 
+if os.getenv("FASTAPI_ENV") is None:
+    logger.info("Run: 'export FASTAPI_ENV=development' in terminal")
+    raise ValueError("FASTAPI_ENV environment variable not set")
 logger = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
 logger.info(f"Logger initialized\tEnvironment: {os.getenv('FASTAPI_ENV')}")
