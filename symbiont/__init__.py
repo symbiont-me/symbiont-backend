c3d51dea922dc1a2c610e3e230dd64fcb4780c10
lonehegelian <thelonehegelian@protonmail.com>
2024-04-05 09:30:55 +0100

    add warning message for production env

--- symbiont/__init__.py
+++ symbiont/__init__.py
@@ -1,8 +1,11 @@
 import os
 from .utils.logger import Logger
 
+logger = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
 if os.getenv("FASTAPI_ENV") is None:
     logger.info("Run: 'export FASTAPI_ENV=development' in terminal")
     raise ValueError("FASTAPI_ENV environment variable not set")
-logger = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
-logger.info(f"Logger initialized\tEnvironment: {os.getenv('FASTAPI_ENV')}")
+if os.getenv("FASTAPI_ENV") == "production":
+    logger.warning("Running in production environment")
+else:
+    logger.warning(f"Environment: {os.getenv('FASTAPI_ENV')}")
