32341393a27555c1914681d43393db6094f26e59
lonehegelian <thelonehegelian@protonmail.com>
2024-04-07 00:01:28 +0100

    get firbase bucket name from storage

--- symbiont/fb/__init__.py
+++ symbiont/fb/__init__.py
@@ -3,18 +3,23 @@ import os
 import json
 import base64
 from .. import logger
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       FIREBASE INIT
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+
+
 if not firebase_admin._apps:
     base_64_cred = os.getenv("FIREBASE_CREDENTIALS")
+    storage_bucket = os.getenv("FIREBASE_STORAGE_BUCKET")
+    logger.info(f"Storage Bucket: {storage_bucket}")
     if not base_64_cred:
         raise ValueError("FIREBASE_CREDENTIALS environment variable not set")
     cred_json = base64.b64decode(base_64_cred).decode("utf-8")
     cred_dict = json.loads(cred_json)
+    logger.debug(cred_dict)
     cred = firebase_admin.credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred, {"storageBucket": "symbiont-e7f06.appspot.com"})
+    firebase_admin.initialize_app(cred, {"storageBucket": storage_bucket})
     firebase_admin.get_app()
     logger.info("Firebase initialized")
