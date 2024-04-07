c4bac74c4a555850b9a8bf5d8fd7f267a8ead085
lonehegelian <thelonehegelian@protonmail.com>
2024-04-07 18:17:24 +0100

    chores: fix linter errors

--- symbiont/llms/__init__.py
+++ symbiont/llms/__init__.py
@@ -6,7 +6,6 @@ from firebase_admin import firestore
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from fastapi import HTTPException
-from pydantic import SecretStr
 import time
 import datetime
 from .. import logger
@@ -20,13 +19,15 @@ def create_prompt(user_query: str, context: str):
         """
         You are a well-informed AI assistant. 
         The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-        AI assistant will take into account any information that is provided and construct a reasonable and well thought response.
+        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly 
+        any question about any topic in conversation.
+        AI assistant will take into account any information that is provided and construct 
+        a reasonable and well thought response.
         START OF INFORMATION {context} END OF INFORMATION
         If it is not enought to provide a reasonable answer to the question, the AI assistant will say:
         "I'm sorry, but I don't know the answer to that question. But my educated opinion would..."
         AI assistant will not invent anything and do its best to provide accurate information.
-        Output Format: Return your answer in valid {output_format} Format
+        Output Format: Return your answer in valid {output_format} format
         {user_query} 
     """
     )
