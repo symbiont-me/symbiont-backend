3fe8f1d0f49543c71f38e9a6bc426e7274a04256
lonehegelian <thelonehegelian@protonmail.com>
2024-04-10 03:33:30 +0100

    BUGFIX: use find_dotenv to load the env files comment: without the find_dotenv load_env was unable to find the configs

--- symbiont/__init__.py
+++ symbiont/__init__.py
@@ -1,14 +1,14 @@
 import os
 from .utils.logger import Logger
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
 logger = Logger(__name__, environment=os.getenv("FASTAPI_ENV"))
 ENVIRONMENT = os.getenv("FASTAPI_ENV", default="development")
 
 if ENVIRONMENT == "production":
-    load_dotenv(".env.production")
+    load_dotenv(find_dotenv(".env.production"))
     logger.critical("Running in production environment")
 if ENVIRONMENT == "development":
-    load_dotenv(".env.development")
+    load_dotenv(find_dotenv(".env.development"))
     logger.info("Running in development environment")
