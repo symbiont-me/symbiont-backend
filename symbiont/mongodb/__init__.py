94519dc1d75e8339bdca611bf4e44a77cc7b6673
lonehegelian <thelonehegelian@protonmail.com>
2024-04-25 01:26:22 +0100

    initialise collections

--- symbiont/mongodb/__init__.py
+++ symbiont/mongodb/__init__.py
@@ -1,15 +1,27 @@
 from pymongo import MongoClient
 from .. import logger
-
-logger.debug("Connecting to MongoDB")
-
-client = MongoClient("localhost", 27017)
-logger.info("Connected to Mongodb")
-
-db = client["symbiont-dev"]
-user_collection = db["users"]
-
-logger.debug(user_collection.find_one())
+import os
+from pydantic import BaseModel
 
 
-# TODO add condition for production
+def init_db_collections(db):
+    studies_collection = db["studies"]
+    users_collection = db["users"]
+    resources_collection = db["resources"]
+    vectors_collection = db["vectors"]
+    return studies_collection, users_collection
+
+
+if os.getenv("FASTAPI_ENV") == "development":
+    client = MongoClient("localhost", 27017)
+    logger.info("Connected to Mongodb")
+    # TODO add to env file
+    db = client["symbiont-dev"]
+    studies_collection, users_collection = init_db_collections(db)
+
+if os.getenv("FASTAPI_ENV") == "production":
+    client = MongoClient(os.getenv("MONGO_URI"), 27017)
+    logger.info("Connected to Mongodb")
+    # TODO add to env file
+    db = client[os.getenv("MONGO_DB_NAME")]
+    studies_collection, users_collection = init_db_collections(db)
