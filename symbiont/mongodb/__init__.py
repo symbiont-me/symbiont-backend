780622065a79778351f9a18294c8eccf1e0b0a76
lonehegelian <thelonehegelian@protonmail.com>
2024-05-17 13:00:02 +0100

    update init process for mongo

--- symbiont/mongodb/__init__.py
+++ symbiont/mongodb/__init__.py
@@ -1,10 +1,8 @@
-from pymongo import MongoClient
 from .. import logger
 import os
-from pydantic import BaseModel
-
 from gridfs import GridFS, GridFSBucket
 import pymongo
+from pymongo.server_api import ServerApi
 
 
 def init_db_collections(db):
@@ -15,30 +13,31 @@ def init_db_collections(db):
     return studies_collection, users_collection
 
 
-if os.getenv("FASTAPI_ENV") == "development":
-    client = None
-    try:
-        client = pymongo.MongoClient("localhost", 27017, serverSelectionTimeoutMS=5000)  # add timeout for connection
-        client.server_info()  # force connection on a request as the
-        # connect=True parameter of MongoClient seems
-        # to be useless here
-        print("Connection to MongoDB was successful")
-    except pymongo.errors.ServerSelectionTimeoutError as err:
-        logger.error("Connection to MongoDB failed")
-    # TODO add to env file
-    if client:
-        db = client["symbiont-dev"]
-        print(db)
-        studies_collection, users_collection = init_db_collections(db)
-        grid_fs = GridFS(db)
-        grid_fs_bucket = GridFSBucket(db)
+mongo_uri = os.getenv("MONGO_URI")
+mongo_port = os.getenv("MONGO_PORT")
+mongo_db_name = os.getenv("MONGO_DB_NAME")
+
+client = None
+
+try:
+    if os.getenv("FASTAPI_ENV") == "development":
+        client = pymongo.MongoClient(
+            mongo_uri, int(mongo_port), serverSelectionTimeoutMS=5000
+        )  # add timeout for connection
+    elif os.getenv("FASTAPI_ENV") == "production":
+        client = pymongo.MongoClient(mongo_uri, server_api=ServerApi("1"))
+    # client.server_info()  # force connection on a request as the
+
+    if client is None:
+        raise pymongo.errors.ServerSelectionTimeoutError
+    client.admin.command("ping")
+    logger.info("Pinged! Connection to MongoDB was successful")
+except pymongo.errors.ServerSelectionTimeoutError as err:
+    logger.error("Connection to MongoDB failed")
 
 
-if os.getenv("FASTAPI_ENV") == "production":
-    client = MongoClient(os.getenv("MONGO_URI"), 27017)
-    logger.info("Connected to Mongodb")
-    # TODO add to env file
-    db = client[os.getenv("MONGO_DB_NAME")]
+if client:
+    db = client[mongo_db_name]
     studies_collection, users_collection = init_db_collections(db)
     grid_fs = GridFS(db)
     grid_fs_bucket = GridFSBucket(db)
