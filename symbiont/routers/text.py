d2ed9fd95457b3091387839567ab74448b56e8dc
AmgadHasan <eng.amghasan@gmail.com>
2024-07-11 00:20:18 +0300

    Update Writer functionality to use MongoDB's studies_collection

--- symbiont/routers/text.py
+++ symbiont/routers/text.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, HTTPException
 from ..models import TextUpdateRequest
-from ..utils.db_utils import StudyService
+from ..mongodb import studies_collection
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       STUDY TEXT
@@ -20,13 +20,16 @@ async def get_text(study_id: str, request: Request):
 
 
 @router.post("/update-text")
-async def update_text(text: TextUpdateRequest, request: Request):
-    user_uid = request.state.verified_user["user_id"]
-    study_service = StudyService(user_uid, text.studyId)
-    study_ref = study_service.get_document_ref()
-    if study_ref is None:
-        raise HTTPException(status_code=404, detail="No such document!")
-    study = study_ref.get()
-    if study.exists:
-        study_ref.update({"text": text.text})
-        return {"message": "Text updated successfully"}
+async def update_text(text_request: TextUpdateRequest, request: Request):
+    studies_collection.update_one({"_id": text_request.studyId}, {"$set": {"text": text_request.text}})
+    
+    return {"message": "Text updated successfully"}
+    # user_uid = request.state.verified_user["user_id"]
+    # study_service = StudyService(user_uid, text.studyId)
+    # study_ref = study_service.get_document_ref()
+    # if study_ref is None:
+    #     raise HTTPException(status_code=404, detail="No such document!")
+    # study = study_ref.get()
+    # if study.exists:
+    #     study_ref.update({"text": text.text})
+    #     return {"message": "Text updated successfully"}
