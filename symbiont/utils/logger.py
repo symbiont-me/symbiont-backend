d1070112c954044c4cb4f315e1cea8089e1d5b3d
AmgadHasan <eng.amghasan@gmail.com>
2024-07-27 04:19:57 +0300

    Disable authenticaion middleware on certain routes e.g. status and docs

--- symbiont/utils/logger.py
+++ symbiont/utils/logger.py
@@ -4,10 +4,8 @@ from colorlog import ColoredFormatter
 
 class Logger:
     def __init__(self, logger_name, log_file=None, environment=None):
-        # Create a logger with the specified name
         self.logger = logging.getLogger(logger_name)
 
-        # Set logger level based on the environment
         if environment and environment.lower() == "production":
             self.logger.setLevel(logging.WARNING)
         else:
@@ -22,15 +20,12 @@ class Logger:
         }
 
         LOGFORMAT = "%(log_color)s%(asctime)-8s%(reset)s | %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"  # noqa: E501
-        # Create a formatter
         color_formatter = ColoredFormatter(LOGFORMAT, log_colors=log_colors)
 
-        # Create a console handler and set the formatter
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(color_formatter)
         self.logger.addHandler(console_handler)
 
-        # If a log file is specified, create a file handler and set the formatter
         if log_file:
             file_handler = logging.FileHandler(log_file)
             file_format = "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
