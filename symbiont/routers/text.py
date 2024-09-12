0ff56dfaaf0c52b53f30d88ae4d9f4d59884ba13
lonehegelian <thelonehegelian@protonmail.com>
2024-09-12 23:54:07 +0100

    feat: add chat completion route to text writer

--- symbiont/routers/text.py
+++ symbiont/routers/text.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request, HTTPException, Cookie
+from fastapi.responses import JSONResponse
 from ..models import TextUpdateRequest
 from ..mongodb import studies_collection
 from symbiont.mongodb.utils import user_exists, check_user_authorization
+from symbiont.llms import init_llm, get_user_llm_settings
+from pydantic import BaseModel, ValidationError
+from typing import Annotated, Optional, Any
+
+from langchain import PromptTemplate
+from langchain_core.messages.ai import AIMessage
+from .. import logger
+from langchain.chains import LLMChain
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       STUDY TEXT
@@ -33,3 +42,38 @@ async def update_text(text_request: TextUpdateRequest, request: Request):
         {"_id": text_request.studyId}, {"$set": {"text": text_request.text}}
     )
     return {"message": "Text updated successfully"}
+
+
+class CompletionRequest(BaseModel):
+    sentence_start: str
+
+
+@router.post("/completion")
+async def completion(
+    completion_request: CompletionRequest,
+    request: Request,
+    api_key: Annotated[str | None, Cookie()] = None,
+):
+    if not completion_request.sentence_start or not api_key:
+        return JSONResponse(
+            {"response": ""},
+        )
+
+    session_data = {
+        "user_id": request.state.session.get_user_id(),
+    }
+
+    user_uid = session_data["user_id"]
+    await user_exists(user_uid)
+    llm_settings = get_user_llm_settings(user_uid)
+    logger.debug(f"Initializing {llm_settings=}")
+    llm = init_llm(llm_settings, api_key)
+    if llm is None:
+        raise HTTPException(status_code=404, detail="No LLM found!")
+
+    template = "Complete the following sentence: {sentence_start}"
+    prompt = PromptTemplate(input_variables=["sentence_start"], template=template)
+    chain = LLMChain(llm=llm, prompt=prompt)
+    llm_response = chain.run(sentence_start=completion_request.sentence_start)
+
+    return JSONResponse({"response": llm_response})
